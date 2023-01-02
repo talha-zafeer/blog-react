03a31051f68847561f6a671b31c7e4f3db7f8486
Zafeer Saadi <talha.zafeer@gigalabs.co>
2023-01-02 21:20:32 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Create.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Mario");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch("");
+  };
+
+  return (
+    <div className="create">
+      <h2>Add a new Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title : </label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label>Blog Body : </label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => {
+            setBody(e.target.value);
+          }}
+        ></textarea>
+        <label>Blog Author : </label>
+        <select
+          value={author}
+          onChange={(e) => {
+            setAuthor(e.target.value);
+          }}
+        >
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button> Add Blog</button>
+      </form>
+      <p>{author}</p>
+    </div>
+  );
+};
+
+export default Create;
